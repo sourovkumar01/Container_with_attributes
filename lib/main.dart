0afdf2c028b1baf96b1ac01c2c36534870de553f
souro <sourovkumarshisir@gmail.com>
2024-03-24 15:02:58 +0600

    Home Screen add

--- lib/main.dart
+++ lib/main.dart
@@ -22,42 +22,80 @@ class HomeScreen extends StatelessWidget {
     // throw UnimplementedError();
     return Scaffold(
       appBar: AppBar(
-        title: Text('Listview'),
+        title: Text('First days'),
         backgroundColor: Colors.green,
         centerTitle: true,
       ),
-      body: Container(
-        height: 250,
-        width: 250,
-        alignment: Alignment.center,
-        margin: EdgeInsets.all(100),
-        padding: EdgeInsets.all(30),
-        decoration: BoxDecoration(
-            color: Colors.white,
-            border: Border.all(color: Colors.cyan, width: 10)),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'First day',
-              style: TextStyle(
-                  color: Colors.black,
-                  fontSize: (25),
-                  fontWeight: FontWeight.bold,
-                  backgroundColor: Colors.green,
-                  decoration: TextDecoration.underline,
-                  wordSpacing: 5),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Container(
+            height: 100,
+            width: 100,
+            margin: EdgeInsets.all(5),
+            padding: EdgeInsets.all(5),
+            color: Colors.deepPurpleAccent,
+            child: Column(
+              children: [
+                Text(
+                  'First day',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: (15),
+                      fontWeight: FontWeight.bold,
+                      backgroundColor: Colors.green,
+                      decoration: TextDecoration.underline,
+                      wordSpacing: 0),
+                ),
+              ],
             ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                primary: Colors.blue,
-              ),
-              child: Text('cleck me '),
+          ),
+          Container(
+            height: 100,
+            width: 100,
+            margin: EdgeInsets.all(5),
+            padding: EdgeInsets.all(5),
+            color: Colors.deepPurpleAccent,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'First day',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: (15),
+                      fontWeight: FontWeight.bold,
+                      backgroundColor: Colors.green,
+                      decoration: TextDecoration.underline,
+                      wordSpacing: 0),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+          Container(
+            height: 100,
+            width: 100,
+            //decoration: BoxDecoration(borderRadius:BorderRadius.circular(2)),
+            margin: EdgeInsets.all(5),
+            padding: EdgeInsets.all(5),
+            color: Colors.deepPurpleAccent,
+            child: Column(
+              children: [
+                Text(
+                  'First day',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: (15),
+                      fontWeight: FontWeight.bold,
+                      backgroundColor: Colors.green,
+                      decoration: TextDecoration.underline,
+                      wordSpacing: 0),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
